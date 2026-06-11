c1bd511edc609223e287b4904ac419acc8d3a089
Uros Mirkovic <mirkovic.uk@gmail.com>
2026-06-11 11:56:03 +0100

    testing: add hello world Next.js page with build config

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <main
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+        fontFamily: "sans-serif",
+        fontSize: "2rem",
+        backgroundColor: "#f0f4f8",
+        color: "#1a202c",
+      }}
+    >
+      Hello World 👋
+    </main>
+  );
+}
